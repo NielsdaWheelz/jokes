430a479dbfeeac1131060c7043716866b666b26b
Niels-Erik Nandal <nielseriknandal@gmail.com>
2025-09-01 12:52:54 -0400

    mapping

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,18 @@
+import jokesData from "./jokesData.js"
 import Joke from "./Joke"
 
-const jokes = [
-  {
-    setup: "I got my daughter a fridge for her birthday.",
-    punchline: "I can't wait to see her face light up when she opens it."
-  },
-  {
-    setup: "How did the hacker escape the police?",
-    punchline: "He just ransomware!"
-  },
-  {
-    setup: "Why don't pirates travel on mountain roads?",
-    punchline: "Scurvy."
-  },
-  {
-    setup: "Why do bees stay in the hive in the winter?",
-    punchline: "Swarm."
-  },
-  {
-    setup: "What's the best thing about Switzerland?",
-    punchline: "I don't know, but the flag is a big plus!"
-  },
-  {
-    punchline: "It's hard to explain puns to kleptomaniacs because they always take things literally."
-  }
-]
+const jokeElements = jokesData.map((joke) => {
+  const setup = joke.setup;
+  const punchline = joke.punchline;
+  return < Joke setup = { setup } punchline = { punchline }/>
+})
 
 export default function App() {
   return (
     <main>
       <h1>Jokes</h1>
       <h2>Here are some jokes:</h2>
-      {jokes.map((joke) => (
-        <Joke setup={joke.setup || ""} punchline={joke.punchline || ""} />
-      ))}
+      { jokeElements }
     </main>
   )
 }

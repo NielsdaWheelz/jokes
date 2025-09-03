5928571ca92882d6bf57b5d3602cf89fb4c182dd
Niels-Erik Nandal <nielseriknandal@gmail.com>
2025-09-03 15:10:54 -0400

    conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import jokesData from "./jokesData.js"
 import Joke from "./Joke"
 
-const jokeElements = jokesData.map((joke) => {
-  const setup = joke.setup;
-  const punchline = joke.punchline;
-  return < Joke setup = { setup } punchline = { punchline }/>
-})
-
 export default function App() {
+
+  const jokeElements = jokesData.map((joke) => {
+    const setup = joke.setup;
+    const punchline = joke.punchline;
+    return < Joke setup = { setup } punchline = { punchline }/>
+  })
+
   return (
     <main>
       <h1>Jokes</h1>
--- src/Joke.tsx
+++ src/Joke.tsx
@@ -1,8 +1,18 @@
+import { useState } from "react"
+
 export default function Joke(props: { setup: string, punchline: string }) {
+
+    const [isShown, setIsShown] = useState(false)
+
+    function togglePunchline() {
+        setIsShown(prevIsShown => !prevIsShown)
+    }
+
     return (
         <>
-            {props.setup && <p>Setup: {props.setup}</p>}
-            {props.punchline && <p>Punchline: {props.punchline}</p>}
+            <h4>Setup: { props.setup }</h4>
+            <button onClick = { togglePunchline }>Toggle Punchline</button>
+            { props.punchline && isShown && <p>Punchline: { props.punchline }</p> }
             <hr />
         </>
     )
